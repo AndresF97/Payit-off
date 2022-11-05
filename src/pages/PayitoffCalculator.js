e3d48b13819abafc79ca719f247c522bf79eac6e
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-11-06 00:05:45 +0900

    added button will work on saving info from user once info has been gotten will restructure form to look more like the wire frame

--- src/pages/PayitoffCalculator.js
+++ src/pages/PayitoffCalculator.js
@@ -11,6 +11,7 @@ function PayitoffCalculator(){
              {/* Form should bring FormWrapper and LoanContainer form the components */}
             <section>
                  <FormWrapper/>
+                 <button className='focus:outline-none text-white font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 calculate-btn'>Calculate <i className="fa-solid fa-money-bill"></i></button>
             </section>
             <br></br>
             <section>
