c4cfce13568dae99b06a25a6244818a384624b3a
AndresF97 <Andres.F.Jimenez.F@gmail.com>
2022-11-07 04:57:06 +0900

    fixed styling for css

--- src/components/LoanConainter/index.js
+++ src/components/LoanConainter/index.js
@@ -93,7 +93,7 @@ function LoanContainer({userLoanInfo,setUserLoanInfo}) {
                     name='payOfMethod'
                     onChange={handleInputChange}
                     value={userLoanInfo.payOfMethod}
-                    className="text-white text-center bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 w-full" 
+                    className="text-dark text-center bg-gray-50 border border-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full" 
                     id="cars">
                         <option
                         value="Snow Ball">
